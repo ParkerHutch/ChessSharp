68bbddf383ffe41cba26adb73362a6d3e11165da
Parker Hutchinson <parker.hutch@outlook.com>
2024-04-16 20:46:28 -0400

    add code to setup board and print it

--- ChessSharp/Class1.cs
+++ ChessSharp/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static ChessSharp.Pawn;
 
 namespace ChessSharp;
 
@@ -13,34 +14,111 @@ public enum PieceType
 	None
 }
 
-public class BoardLocation
+public class BoardLocation(int row, int col)
 {
-	public int Row { get; set; }
-	public int Col { get; set; }
+	public int Row { get; set; } = row;
+	public int Col { get; set; } = col;
 }
 public class Move
 {
 
 }
 
-public interface IPiece
+public interface IPiece // use abstract base class?
 {
-	BoardLocation Location { get; set; }
-	PieceType Type { get; }
-	IEnumerable<Move> GetValidMoves(Board board);
+    BoardLocation Location { get; set; }
+    PieceType Type { get; }
+    public IEnumerable<Move> GetValidMoves(Board board);
+
+	//public override string ToString; // need to use an abstract base class: https://stackoverflow.com/questions/510341/force-subclasses-of-an-interface-to-implement-tostring
+	public string getNotation();
 }
+
+public class Pawn(int row, int col) : IPiece
+{
+	public PieceType Type => PieceType.Pawn;
+
+	BoardLocation IPiece.Location { get; set; } = new(row, col);
+	public PieceType type { get; } = PieceType.Pawn;
+
+	public IEnumerable<Move> GetValidMoves(Board board)
+	{
+		List<Move> validMoves = new List<Move>();
+		return validMoves;
+	}
+
+    public override string ToString()
+    {
+		return "P";
+    }
+
+    public string getNotation()
+    {
+		return "";
+    }
+}
+
+
+
+
 public class Board
 {
-	private class Square(int row, int col, IPiece? piece = null)
+	public class Square(int row, int col, IPiece? piece = null)
 	{
-		public int Row { get; } = row;
-		public int Col { get; } = col;
+		public BoardLocation Location { get; set; } = new(row, col);
 		public IPiece? Piece { get; set; } = piece;
-	}
-	private static Square[,] _boardArr = new Square[8, 8];
 
+        public override string ToString()
+        {
+            return Piece?.ToString() ?? "-";
+        }
+    }
+	public static Square[,] BoardArr = new Square[8, 8];
+
+	//private static Square[,] _boardArr = new Square[8, 8]
+	//{
+	//	new Square[8] {},
+	//	new Square[8] { },
+	//       new Square[8] { },
+	//       new Square[8] { },
+	//       new Square[8] { },
+	//       new Square[8] { },
+	//       new Square[8] { },
+	//       new Square[8] { }
+	//   }
+
+	private static void SetBoard()
+	{
+		for (int row = 0; row < 8; ++row)
+		{
+			for (int col = 0; col < 8; ++col)
+			{
+				BoardArr[row, col] = new Square(row, col);
+			}
+		}
+	}
 	public Board()
 	{
-
+		SetBoard();
 	}
+
+	public void Print()
+	{
+		string topRowNumbers = "\t" + string.Join("\t", Enumerable.Range(1, 8));
+		Console.WriteLine(topRowNumbers);
+
+        for (int row = 0; row < 8; ++row)
+        {
+			Console.Write($"{row + 1}\t");
+            for (int col = 0; col < 8; ++col)
+            {
+				Console.Write($"{BoardArr[row, col]}\t");
+            }
+            Console.WriteLine();
+
+        }
+    }
 }
+
+
+
--- ChessSharp/Program.cs
+++ ChessSharp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ChessSharp;
+
 Console.WriteLine("Hello, World!");
+
+
+Board board = new();
+
+board.Print();
+
