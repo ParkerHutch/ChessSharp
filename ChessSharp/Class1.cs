9a870a4f2b40585a95c7d184dad1a054f77afb04
Parker Hutchinson <parker.hutch@outlook.com>
2024-04-16 20:00:47 -0400

    add initial layout code

---
+++ ChessSharp/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ChessSharp;
+
+public enum PieceType
+{
+	King,
+	Queen,
+	Rook,
+	Bishop,
+	Knight,
+	Pawn,
+	None
+}
+
+public class BoardLocation
+{
+	public int Row { get; set; }
+	public int Col { get; set; }
+}
+public class Move
+{
+
+}
+
+public interface IPiece
+{
+	BoardLocation Location { get; set; }
+	PieceType Type { get; }
+	IEnumerable<Move> GetValidMoves(Board board);
+}
+public class Board
+{
+	private class Square(int row, int col, IPiece? piece = null)
+	{
+		public int Row { get; } = row;
+		public int Col { get; } = col;
+		public IPiece? Piece { get; set; } = piece;
+	}
+	private static Square[,] _boardArr = new Square[8, 8];
+
+	public Board()
+	{
+
+	}
+}
