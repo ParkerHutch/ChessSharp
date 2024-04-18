ef7ac98ff43a81e0c8fc7263fa7793600005e70c
Parker Hutchinson <parker.hutch@outlook.com>
2024-04-18 12:17:48 -0400

    add initial in-progress GUI work

---
+++ ChessSharpGUI/Images.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ChessSharpGUI
+{
+    public static class Images
+    {
+        public readonly static ImageSource Empty = LoadImage("king_wikimedia_commons.png");
+        public readonly static ImageSource King = LoadImage("king_wikimedia_commons.png");
+        private static ImageSource LoadImage(string filename)
+        {
+            return new BitmapImage(new Uri($"Assets/{filename}", UriKind.Relative));
+        }
+    }
+}
