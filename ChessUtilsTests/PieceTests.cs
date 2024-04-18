7fe639c549ecdb3f459d4952e8283fb92e797a72
Parker Hutchinson <parker.hutch@outlook.com>
2024-04-18 11:54:49 -0400

    add tests for queen moves

--- ChessUtilsTests/PieceTests.cs
+++ ChessUtilsTests/PieceTests.cs
@@ -360,7 +360,7 @@ namespace ChessUtilsTests
     [TestClass]
     public class TestQueenMoves
     {
-        Board board = new(true);
+        public Board board = new(true);
 
         [TestInitialize]
         public void setupBoard()
@@ -368,12 +368,265 @@ namespace ChessUtilsTests
             board = new(true);
         }
 
-        [TestMethod]
-        public void TestValidMoves()
+        [TestClass]
+        public class TestQueenHasKingMoves()
         {
-            // TODO use bishop and rook tests here first then add king tests
-            Assert.IsTrue(false);
+            public Board board = new(true);
+
+            [TestInitialize]
+            public void setupBoard()
+            {
+                board = new(true);
+            }
+
+            [TestMethod]
+            public void TestCanMoveToEmptySquares()
+            {
+                Board.Square queenSquare = board.BoardArr[3, 4];
+                queenSquare.Piece = new Queen(queenSquare.Location.Row, queenSquare.Location.Col, Color.White);
+
+                for (int row = queenSquare.Location.Row - 1; row <= queenSquare.Location.Row + 1; row++)
+                {
+                    for (int col = queenSquare.Location.Col - 1; col <= queenSquare.Location.Col + 1; col++)
+                    {
+                        if (row == queenSquare.Location.Row && col == queenSquare.Location.Col)
+                        {
+                            continue;
+                        }
+                        if (board.getSquareAt(row, col, out Board.Square? neighborSquare))
+                        {
+                            CustomCollectionAsserter.Contains
+                                (queenSquare.Piece.GetValidMoves(board),
+                                new Move(queenSquare, neighborSquare!));
+                        }
+                    }
+                }
+            }
+            [TestMethod]
+            public void TestCanMoveToEnemyOccupiedSquares()
+            {
+                Board.Square queenSquare = board.BoardArr[3, 4];
+                queenSquare.Piece = new Queen(queenSquare.Location.Row, queenSquare.Location.Col, Color.White);
+
+                for (int row = queenSquare.Location.Row - 1; row <= queenSquare.Location.Row + 1; row++)
+                {
+                    for (int col = queenSquare.Location.Col - 1; col <= queenSquare.Location.Col + 1; col++)
+                    {
+                        if (row == queenSquare.Location.Row && col == queenSquare.Location.Col)
+                        {
+                            continue;
+                        }
+                        if (board.getSquareAt(row, col, out Board.Square? neighborSquare))
+                        {
+                            neighborSquare!.Piece = new Pawn(row, col, Color.Black);
+                            CustomCollectionAsserter.Contains
+                                (queenSquare.Piece.GetValidMoves(board),
+                                new Move(queenSquare, neighborSquare!));
+                        }
+                    }
+                }
+            }
+
+            [TestMethod]
+            public void TestCannotMoveToFriendlyOccupiedSquares()
+            {
+                Board.Square queenSquare = board.BoardArr[3, 4];
+                queenSquare.Piece = new Queen(queenSquare.Location.Row, queenSquare.Location.Col, Color.White);
+
+                for (int row = queenSquare.Location.Row - 1; row <= queenSquare.Location.Row + 1; row++)
+                {
+                    for (int col = queenSquare.Location.Col - 1; col <= queenSquare.Location.Col + 1; col++)
+                    {
+                        if (row == queenSquare.Location.Row && col == queenSquare.Location.Col)
+                        {
+                            continue;
+                        }
+                        if (board.getSquareAt(row, col, out Board.Square? neighborSquare))
+                        {
+                            neighborSquare!.Piece = new Pawn(row, col, Color.White);
+                            CustomCollectionAsserter.DoesNotContain
+                                (queenSquare.Piece.GetValidMoves(board),
+                                new Move(queenSquare, neighborSquare!));
+                        }
+                    }
+                }
+            }
         }
+
+        [TestClass]
+        public class TestQueenHasRookMoves
+        {
+            Board board = new(true);
+
+            [TestInitialize]
+            public void setupBoard()
+            {
+                board = new(true);
+            }
+
+            [TestMethod]
+            public void TestCanMoveForwardUntilOccupiedByFriendlyPiece()
+            {
+                int lowerRow = 2;
+                int upperRow = 5;
+                Board.Square lowerQueenSquare = board.BoardArr[lowerRow, 0];
+                Board.Square upperQueenSquare = board.BoardArr[upperRow, 0];
+
+                lowerQueenSquare.Piece = new Queen(lowerRow, 0, Color.White);
+                upperQueenSquare.Piece = new Queen(upperRow, 0, Color.White);
+
+                // verify that both Queens can move to any empty square between them
+                for (int row = lowerRow + 1; row < upperRow; ++row)
+                {
+                    Move lowerQueenMove = new(lowerQueenSquare, board.BoardArr[row, 0]);
+
+                    CustomCollectionAsserter.Contains(lowerQueenSquare.Piece.GetValidMoves(board), lowerQueenMove);
+
+                    Move upperQueenMove = new(upperQueenSquare, board.BoardArr[row, 0]);
+                    CustomCollectionAsserter.Contains(upperQueenSquare.Piece.GetValidMoves(board), upperQueenMove);
+                }
+
+                // verify that Queens can't move on top of or past each other
+                for (int row = upperRow; row < 8; ++row)
+                {
+                    Move invalidLowerQueenMove = new(lowerQueenSquare, board.BoardArr[row, 0]);
+                    CustomCollectionAsserter.DoesNotContain(lowerQueenSquare.Piece.GetValidMoves(board), invalidLowerQueenMove);
+                }
+
+                for (int row = lowerRow; row >= 0; --row)
+                {
+                    Move invalidUpperQueenMove = new(upperQueenSquare, board.BoardArr[row, 0]);
+                    CustomCollectionAsserter.DoesNotContain(upperQueenSquare.Piece.GetValidMoves(board), invalidUpperQueenMove);
+                }
+            }
+
+            [TestMethod]
+            public void TestHorizontalMovesToUnoccupiedSquares()
+            {
+                Board.Square queenSquare = board.BoardArr[0, 4];
+
+                queenSquare.Piece = new Queen(0, 4, Color.White);
+                for (int col = 0; col < 8; ++col)
+                {
+                    if (col == queenSquare.Location.Col)
+                    {
+                        continue;
+                    }
+                    Move horizontalQueenMove = new Move(queenSquare, board.BoardArr[queenSquare.Location.Row, col]);
+                    CustomCollectionAsserter.Contains(queenSquare.Piece.GetValidMoves(board), horizontalQueenMove);
+                }
+
+            }
+            [TestMethod]
+            public void TestVerticalMovesToUnoccupiedSquares()
+            {
+                Board.Square queenSquare = board.BoardArr[4, 0];
+
+                queenSquare.Piece = new Queen(4, 0, Color.White);
+                for (int row = 0; row < 8; ++row)
+                {
+                    if (row == queenSquare.Location.Row)
+                    {
+                        continue;
+                    }
+                    Move horizontalQueenMove = new Move(queenSquare, board.BoardArr[row, queenSquare.Location.Col]);
+                    CustomCollectionAsserter.Contains(queenSquare.Piece.GetValidMoves(board), horizontalQueenMove);
+                }
+
+            }
+        }
+
+        [TestClass]
+        public class TestQueenHasBishopMoves
+        {
+            Board board = new(true);
+
+            [TestInitialize]
+            public void setupBoard()
+            {
+                board = new(true);
+            }
+
+            [TestMethod]
+            public void TestCanMoveDiagonalUntilSquareOccupiedByFriendlyPiece()
+            {
+                // bishop sandwiched by other friendly pieces
+                Board.Square queenSquare = board.BoardArr[3, 4]; // equivalent to E4
+
+                queenSquare.Piece = new Bishop(queenSquare.Location.Row, queenSquare.Location.Col, Color.White);
+
+                int pawnRow = 5;
+                board.BoardArr[pawnRow, 2].Piece = new Pawn(pawnRow, 2, Color.White);
+                board.BoardArr[pawnRow, 6].Piece = new Pawn(pawnRow, 6, Color.White);
+
+                // check the bottom-left to upper-right diagonal moves are valid
+                for (int row = 0; row < queenSquare.Location.Row; ++row)
+                {
+                    Move lowerDiagonalLeftMove = new(queenSquare, board.BoardArr[row, row + 1]);
+                    Move lowerDiagonalRightMove = new(queenSquare, board.BoardArr[row, 7 - row]);
+                    CustomCollectionAsserter.Contains(queenSquare.Piece.GetValidMoves(board), lowerDiagonalLeftMove);
+                    CustomCollectionAsserter.Contains(queenSquare.Piece.GetValidMoves(board), lowerDiagonalRightMove);
+                }
+
+                for (int row = queenSquare.Location.Row + 1; row < pawnRow; ++row)
+                {
+                    int squaresAboveBishopCount = row - queenSquare.Location.Row;
+                    Move upperDiagonalLeftMove = new(queenSquare, board.BoardArr[row, queenSquare.Location.Col - squaresAboveBishopCount]);
+                    Move upperDiagonalRightMove = new(queenSquare, board.BoardArr[row, queenSquare.Location.Col + squaresAboveBishopCount]);
+                    CustomCollectionAsserter.Contains(queenSquare.Piece.GetValidMoves(board), upperDiagonalLeftMove);
+                    CustomCollectionAsserter.Contains(queenSquare.Piece.GetValidMoves(board), upperDiagonalRightMove);
+                }
+
+                // check that moves on or after the pawns are not valid
+                for (int row = pawnRow; row < 8; ++row)
+                {
+                    int squaresAboveBishopCount = row - queenSquare.Location.Row;
+                    if (board.getSquareAt(row, queenSquare.Location.Col - squaresAboveBishopCount, out Board.Square? northwestDiagonalSquare))
+                    {
+                        Move invalidNorthwestMove = new(queenSquare, northwestDiagonalSquare!);
+                        CustomCollectionAsserter.DoesNotContain(queenSquare.Piece.GetValidMoves(board), invalidNorthwestMove);
+                    }
+                    if (board.getSquareAt(row, queenSquare.Location.Col + squaresAboveBishopCount, out Board.Square? northeastDiagonalSquare))
+                    {
+                        Move invalidNortheastMove = new(queenSquare, northeastDiagonalSquare!);
+                        CustomCollectionAsserter.DoesNotContain(queenSquare.Piece.GetValidMoves(board), invalidNortheastMove);
+                    }
+                }
+            }
+
+            [TestMethod]
+            public void TestCanMoveToDiagonalSquareIfOccupiedByEnemy()
+            {
+                // bishop sandwiched by other friendly pieces
+                Board.Square queenSquare = board.BoardArr[3, 4]; // equivalent to E4
+
+                queenSquare.Piece = new Queen(queenSquare.Location.Row, queenSquare.Location.Col, Color.White);
+
+                int offset = 1;
+
+                List<Board.Square> enemyPawnSquares = new List<Board.Square>
+            {
+                board.BoardArr[queenSquare.Location.Row - offset,
+                    queenSquare.Location.Col - offset],
+                board.BoardArr[queenSquare.Location.Row - offset,
+                    queenSquare.Location.Col + offset],
+                board.BoardArr[queenSquare.Location.Row + offset,
+                    queenSquare.Location.Col - offset],
+                board.BoardArr[queenSquare.Location.Row + offset,
+                    queenSquare.Location.Col + offset],
+
+            };
+                foreach (Board.Square enemyPawnSquare in enemyPawnSquares)
+                {
+                    enemyPawnSquare.Piece = new Pawn(enemyPawnSquare.Location.Row,
+                        enemyPawnSquare.Location.Col, Color.Black);
+                    CustomCollectionAsserter.Contains(queenSquare.Piece.GetValidMoves(board),
+                        new Move(queenSquare, enemyPawnSquare));
+                }
+            }
+        }
+
+
     }
 
     [TestClass]
